f429aff0e43398bdea9dc1575bd196eea45a244a
Ludwig Kratzl <kratzl.ludwig@gmail.com>
2021-03-23 09:31:27 +0100

    Fix rand and buffer address

--- src/DynamicReceive.cpp
+++ src/DynamicReceive.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <mpi.h>
 #include <stdio.h>
+#include <time.h>
 #include <vector>
 
 int main(int argc, char *argv[]) {
@@ -12,6 +13,7 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
   int max = 100;
+  srand(time(NULL));
   int length = (rand() % max) + 1;
   std::vector<int> buffer(length);
 
@@ -20,17 +22,20 @@ int main(int argc, char *argv[]) {
   }
 
   if (world_rank == 0) {
-    printf("Process %d sends %d elements to %d\n", world_rank, length, world_rank + 1);
-    MPI_Send(&buffer, length, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    printf("Process %d sends %d elements to %d\n", world_rank, length,
+           world_rank + 1);
+    MPI_Send(&buffer[0], length, MPI_INT, 1, 0, MPI_COMM_WORLD);
     printf("Process %d continues its work\n", world_rank);
   } else if (world_rank == 1) {
     int received_length;
     MPI_Status status;
-    //we receive at most max numbers!
-    MPI_Recv(&buffer, max, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    // we receive at most max numbers!
+    MPI_Recv(&buffer[0], max, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG,
+             MPI_COMM_WORLD, &status);
     MPI_Get_count(&status, MPI_INT, &received_length);
 
-    printf("Process %d received %d elements from process %d\n", world_rank, received_length, status.MPI_SOURCE);
+    printf("Process %d received %d elements from process %d\n", world_rank,
+           received_length, status.MPI_SOURCE);
   }
 
   printf("Process %d continues its work\n", world_rank);
